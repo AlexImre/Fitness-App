aff9ad4ddc0953f02ee5637ca7dd748575e4392c
Alex Imre <alexandre.imre@gmail.com>
2022-10-21 12:34:05 +0100

    style Register page

--- Client/src/App.js
+++ Client/src/App.js
@@ -29,6 +29,7 @@ function App() {
     <Router>
       <Routes>
         <Route path="/" element={ <Login /> } />
+        <Route path="/Login" element={ <Login /> } />
         <Route path="/Register" element={ <Register /> } />
         <Route path="/Home" element={
           <Home 
--- Client/src/Pages/Register.js
+++ Client/src/Pages/Register.js
@@ -1,7 +1,7 @@
-import React, { useState } from 'react'
+import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
 
 export const Register = () => {
-
     // https://jasonwatmore.com/post/2020/02/01/react-fetch-http-post-request-examples
     const [username, setUsername] = useState();
     const [password, setPassword] = useState();
@@ -16,12 +16,23 @@ export const Register = () => {
     }
 
     return (
-        <div className='RegisterContainer'>
+        <div className='LoginContainer'>
             <form method="post" action="register">
-                <h1>Register Page</h1>
-                <input type="text" name="uname" placeholder='Enter username' onChange={setUsername} />
-                <input type="password" name="pw" placeholder='Enter password' onChange={setPassword} />
-                <input type="submit" value="Submit" onClick={registerAccount} />
+                <div className='LoginWrapper'>
+                    <div className='LoginLogo'>
+                        <i className="fa-solid fa-person-running"></i>
+                    </div>
+                    <h1 className='LoginTitle'>Fitr</h1>
+                    <div className='LoginAccess'>
+                        <span className='LoginPrompt'>Create new account</span>
+                        <span className='LoginUserNameText'>Username</span>
+                        <input className='LoginInput' type="text" name="uname" onChange={setUsername} />
+                        <span className='LoginPasswordText'>Password</span>
+                        <input className='LoginInput' type="password" name="pw" onChange={setPassword} />
+                        <button className='button-1' onClick={registerAccount}>Create</button>
+                        <span className='LoginRegisterPrompt'>Already have an account? <Link to="/Login" className='LoginRegisterLink'><strong>Login</strong></Link></span>
+                    </div>
+                </div>
             </form>
         </div>
     )
